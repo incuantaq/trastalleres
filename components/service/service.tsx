c1d43a43d5cad943a6894311413a407d388a30be
Andrés Santiago Romero <incuantaqdev@gmail.com>
2025-02-16 18:21:56 -0500

    feat(trastalleres): add error handler component

--- components/service/service.tsx
+++ components/service/service.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Avatar from "../../app/avatar";
 import DateComponent from "../../app/date";
 import CoverImage from "../../app/cover-image";
+import ErrorHandler from "../ErrorHandler/ErrorHandler";
 
 type ServiceKeys = 'libreria' | 'galeria';
 
@@ -15,6 +16,20 @@ const serviceInfo = {
     serviceDescription: "Explora la galería artística seleccionada por Hugo, donde cada obra resalta la creatividad y la emoción. Un espacio que celebra el arte contemporáneo, invitando a la admiración y a la inspiración."
   }
 }
+
+type Post = {
+  title: string;
+  coverImage: {
+    url: string;
+  };
+  date: string;
+  excerpt: string;
+  author: {
+    name: string;
+    picture: string;
+  };
+  slug: string;
+};
 
 function PostPreview({
   title,
@@ -47,30 +62,38 @@ function PostPreview({
   );
 }
 
-export default function Service({ posts, serviceType }: { posts: any[], serviceType: ServiceKeys }) {
-  const {serviceTitle, serviceDescription } = serviceInfo[serviceType];
+export default function Service({ posts, serviceType }: { posts: Post[] | null; serviceType: ServiceKeys }) {
+  const { serviceTitle, serviceDescription } = serviceInfo[serviceType];
+
   return (
     <section className="px-16 md:px-20 py-14">
       <h2 className="mb-8 text-4xl md:text-5xl font-bold tracking-tighter leading-tight">
         {serviceTitle}
       </h2>
-      <span className="text-2xl md:text-2xl  tracking-tighter ">
+      <span className="text-2xl md:text-2xl tracking-tighter">
         {serviceDescription}
       </span>
-      <div className="grid grid-cols-1 md:40 grid-cols-1 md:grid-cols-3 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32 py-6">
-        {posts.map((post) => (
-          <PostPreview
-            key={post.slug}
-            title={post.title}
-            coverImage={post.coverImage}
-            date={post.date}
-            author={post.author}
-            slug={post.slug}
-            excerpt={post.excerpt}
-            serviceType={serviceType}
+      {!posts || posts.length === 0 ? (
+          <ErrorHandler
+            message="Oops! Estamos preparando nuevo contenido para ti"
           />
-        ))}
-      </div>
+        ) : (
+          <div className="grid grid-cols-1 md:40 grid-cols-1 md:grid-cols-3 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32 py-6">
+              { posts.map((post) => (
+                  <PostPreview
+                    key={post.slug}
+                    title={post.title}
+                    coverImage={post.coverImage}
+                    date={post.date}
+                    author={post.author}
+                    slug={post.slug}
+                    excerpt={post.excerpt}
+                    serviceType={serviceType}
+                  />
+              ))
+            }
+          </div>
+        )}
     </section>
   );
 }
